24a76d3080f3024d72e579121cc852ba7c638c35
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-09-07 09:44:53 +0700

    project  error condition, empty, and loading
    export project card to a component

---
+++ src/components/Project/PCard.tsx
@@ -0,0 +1,82 @@
+import { Center, Title, Card, Text, Stack, Button, ActionIcon, createStyles, Badge, ScrollArea } from "@mantine/core";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { linkIcon } from "../../interfaces/db";
+import { iconMap } from "../Admin/Project";
+
+const useStyles = createStyles((theme) => ({
+	cardLinks: {
+		position: "absolute",
+		bottom: "10px",
+		left: 0,
+		right: 0,
+		marginLeft: "auto",
+		marginRight: "auto",
+		textAlign: "center",
+	},
+}));
+
+interface IProjectCardProps {
+	title: string;
+	desc: string;
+	tags: string[];
+	links: linkIcon[];
+	btnReloadFunction?: () => void;
+}
+
+export const PCard = ({ title, desc, links, tags, btnReloadFunction }: IProjectCardProps) => {
+	const { classes } = useStyles();
+
+	return (
+		<motion.div whileHover={{ y: "-7px", transition: { duration: 0.2 } }}>
+			<Card shadow="lg" p="lg" radius="md" withBorder sx={{ maxWidth: "350px" }} className="relative">
+				<Stack justify="space-between">
+					<Stack>
+						<Center mb={"xs"}>
+							<Title order={5}>{title}</Title>
+						</Center>
+
+						<Center mb={"xs"}>
+							<Text size="sm" color="dimmed" className="center-text">
+								{desc}
+							</Text>
+						</Center>
+
+						<ScrollArea type="hover" scrollHideDelay={300} scrollbarSize={6} mb="1.5rem">
+							<Center mb={"sm"}>
+								{tags.map((tag) => (
+									<Badge key={tag} mx={4}>
+										{tag}
+									</Badge>
+								))}
+							</Center>
+						</ScrollArea>
+					</Stack>
+
+					<Center className={classes.cardLinks}>
+						{links &&
+							links.length > 0 &&
+							links.map((links) => {
+								const LinkIcon = iconMap[links.type];
+								return (
+									<span className="subtle-link" key={links.url}>
+										<Link href={links.url} passHref>
+											<ActionIcon size="lg" component="a" target="_blank" rel="noopener noreferrer">
+												<LinkIcon stroke={1.5} />
+											</ActionIcon>
+										</Link>
+									</span>
+								);
+							})}
+					</Center>
+
+					{btnReloadFunction && (
+						<Button variant="light" color="blue" fullWidth mt="md" radius="md" onClick={btnReloadFunction}>
+							Reload
+						</Button>
+					)}
+				</Stack>
+			</Card>
+		</motion.div>
+	);
+};
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,67 +1,55 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import { Center, Title, Card, Text, Stack, SimpleGrid, LoadingOverlay, ActionIcon, createStyles, Badge, ScrollArea, Group } from "@mantine/core";
+import { Center, Title, Stack, SimpleGrid, LoadingOverlay } from "@mantine/core";
 import { Wrapper } from "../Utils/Template/Wrapper";
 import { SERVER_V1 } from "../../helper";
 import { IProject } from "../../interfaces/db";
-import { iconMap } from "../Admin/Project";
-import Link from "next/link";
-
-const useStyles = createStyles((theme) => ({
-	cardLinks: {
-		position: "absolute",
-		bottom: "10px",
-		left: 0,
-		right: 0,
-		marginLeft: "auto",
-		marginRight: "auto",
-		textAlign: "center",
-	},
-}));
+import { PCard } from "./PCard";
 
 const title = "Projects | Dadangdut33",
 	desc = "Showcase of some of my projects or things that i have made on my free time";
 
 export const Project: NextPage = (props) => {
-	const { classes } = useStyles();
 	const [projects, setProjects] = useState<IProject[]>([]);
 	const [loading, setLoading] = useState(true);
 	const [loadFail, setLoadFail] = useState(false);
 	const [failMsg, setFailMsg] = useState("");
 	const [maxCol, setMaxCol] = useState(1);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const req = await fetch(SERVER_V1 + "/project", {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				});
+	const fetchData = async () => {
+		setLoading(true);
+		try {
+			const req = await fetch(SERVER_V1 + "/project", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
 
-				const res = await req.json();
-				if (req.status !== 200) {
-					setLoading(false);
-					setLoadFail(true);
-					setFailMsg(res.message);
-				} else {
-					setLoading(false);
-					setProjects(res.data);
+			const res = await req.json();
 
-					if (res.data.length <= 1) setMaxCol(1);
-					else if (res.data.length % 2 === 0) setMaxCol(2);
-					else setMaxCol(3);
-				}
-			} catch (error: any) {
+			if (req.status !== 200) {
 				setLoading(false);
 				setLoadFail(true);
-				setFailMsg(error.message);
-			}
-		};
+				setFailMsg(res.message);
+			} else {
+				setLoading(false);
+				setLoadFail(false);
+				setProjects(res.data);
 
+				if (res.data.length <= 1) setMaxCol(1);
+				else if (res.data.length % 2 === 0) setMaxCol(2);
+				else setMaxCol(3);
+			}
+		} catch (error: any) {
+			setLoading(false);
+			setLoadFail(true);
+			setFailMsg(error.message);
+		}
+	};
+
+	useEffect(() => {
 		fetchData();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
@@ -109,7 +97,7 @@ export const Project: NextPage = (props) => {
 						<Center className="relative" mt={"md"}>
 							<LoadingOverlay visible={loading} overlayBlur={3} />
 							<SimpleGrid
-								cols={maxCol}
+								cols={loadFail ? 1 : maxCol}
 								sx={{ width: "95%" }}
 								breakpoints={[
 									{ maxWidth: "md", cols: 3, spacing: "md" },
@@ -118,52 +106,17 @@ export const Project: NextPage = (props) => {
 								]}
 								spacing="lg"
 							>
-								{projects.map((project) => (
-									<motion.div whileHover={{ y: "-7px", transition: { duration: 0.2 } }} key={project._id}>
-										<Card shadow="lg" p="lg" radius="md" withBorder sx={{ maxWidth: "350px" }} className="relative">
-											<Stack justify="space-between">
-												<Stack>
-													<Center mb={"xs"}>
-														<Title order={5}>{project.title}</Title>
-													</Center>
-
-													<Center mb={"xs"}>
-														<Text size="sm" color="dimmed" className="center-text">
-															{project.description}
-														</Text>
-													</Center>
-
-													<ScrollArea type="hover" scrollHideDelay={300} scrollbarSize={6} mb="1.5rem">
-														<Center mb={"sm"}>
-															{project.tags.map((tag) => (
-																<Badge key={tag} mx={4}>
-																	{tag}
-																</Badge>
-															))}
-														</Center>
-													</ScrollArea>
-												</Stack>
-
-												<Center className={classes.cardLinks}>
-													{project.links && project.links.length > 0
-														? project.links.map((links) => {
-																const LinkIcon = iconMap[links.type];
-																return (
-																	<span className="subtle-link" key={links.url}>
-																		<Link href={links.url} passHref>
-																			<ActionIcon size="lg" component="a" target="_blank" rel="noopener noreferrer">
-																				<LinkIcon stroke={1.5} />
-																			</ActionIcon>
-																		</Link>
-																	</span>
-																);
-														  })
-														: "None"}
-												</Center>
-											</Stack>
-										</Card>
-									</motion.div>
-								))}
+								{projects.length > 0 ? (
+									projects.map((project) => <PCard key={project._id} title={project.title} desc={project.description} tags={project.tags} links={project.links} />)
+								) : (
+									<>
+										{loadFail ? (
+											<PCard title="Fail to Load" desc={failMsg} tags={[]} links={[]} btnReloadFunction={() => fetchData()} />
+										) : (
+											<PCard title="No Projects" desc="Projects have not been added yet" tags={[]} links={[]} />
+										)}
+									</>
+								)}
 							</SimpleGrid>
 						</Center>
 					</Stack>
