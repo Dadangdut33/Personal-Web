a9a0bcefb7751178c88661b025ec1d24374ef0d6
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-09-21 22:13:39 +0700

    Update constants.ts

--- src/helper/global/constants.ts
+++ src/helper/global/constants.ts
@@ -6,7 +6,7 @@
  * server
  */
 // https://api.dadangdut33.codes
-export const SERVER = ___prod___ ? "https://witty-fox-train.cyclic.app" : "http://localhost:42069";
+export const SERVER = ___prod___ ? "https://api.dadangdut33.codes" : "http://localhost:42069";
 export const SERVER_V1 = SERVER + "/v1";
 export const SERVER_LOCAL = ___prod___ ? "https://dadangdut33.codes/api" : "http://localhost:3000/api";
 export const SERVER_LOCAL_V1 = SERVER_LOCAL + "/v1";
