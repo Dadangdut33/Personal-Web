02f21334c1886c852bf10e1cf8179d56de5e0d55
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2024-02-29 16:52:43 +0700

    Update constants.ts

--- src/helper/global/constants.ts
+++ src/helper/global/constants.ts
@@ -9,7 +9,7 @@
 export const domain = ".codes";
 export const analyticsPublicId = "lWYjsDUg";
 export const trackingId = "ea4851ff-e1ec-48f7-be28-7e17c58aa147";
-export const SERVER = ___prod___ ? `https://witty-fox-train.cyclic.app` : "http://localhost:42069";
+export const SERVER = ___prod___ ? `https://expensive-gaiters-eel.cyclic.app/` : "http://localhost:42069";
 export const SERVER_V1 = SERVER + "/v1";
 export const SERVER_LOCAL = ___prod___ ? `https://dadangdut33${domain}/api` : "http://localhost:3000/api";
 export const SERVER_LOCAL_V1 = SERVER_LOCAL + "/v1";
