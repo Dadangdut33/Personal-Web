c2f359fe8c514406e4b0bc51ddc78257adc74fc2
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2026-03-03 16:36:48 +0700

    fix: remove unused imports

--- app/controllers/media.controller.ts
+++ app/controllers/media.controller.ts
@@ -7,10 +7,8 @@ import { MediaTransformer } from '#transformers/media.transformer'
 import { PaginationMeta } from '#types/app'
 import { mediaUploadAPIValidator } from '#validators/media'
 
-import cache from '@adonisjs/cache/services/main'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
-import logger from '@adonisjs/core/services/logger'
 
 @inject()
 export default class MediaController {
